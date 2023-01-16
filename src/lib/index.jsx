8efbcefd0a2543765542bc1293d0108c20a742bd
Calixte <tchicoulo@hotmail.fr>
2023-01-16 17:15:14 +0100

    Added new version with better css properties

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import { React } from "react";
 import Cross from "./img/Cross.svg";
 import PropTypes from "prop-types";
 
@@ -11,8 +11,7 @@ const ModalDialog = (props) => {
     zIndex: 5,
     width: "100%",
     height: "100%",
-    backgroundColor: "rgb(64, 64, 64)",
-    opacity: 0.8,
+    backgroundColor: "rgba(64, 64, 64, 0.8)",
   };
 
   let styleBackground = { ...modalBackground, ...props.background };
@@ -39,30 +38,19 @@ const ModalDialog = (props) => {
     cursor: "pointer",
   };
 
-  const [modal, setModal] = useState(true);
-
-  const closeModal = () => {
-    setModal(false);
-  };
-
   return (
     <div className="modal">
-      {modal ? (
-        <div style={styleBackground} onClick={closeModal}>
-          <div style={styleContainerModal}>
-            <p style={props.styleText}>{props.text}</p>
-
-            <img
-              src={Cross}
-              alt="cross-icon"
-              onClick={closeModal}
-              style={modalBtn}
-            />
-          </div>
+      <div style={styleBackground} onClick={props.handleClick}>
+        <div onClick={(e) => e.stopPropagation()} style={styleContainerModal}>
+          <p style={props.styleText}>{props.text}</p>
+          <img
+            src={Cross}
+            alt="cross-icon"
+            onClick={props.handleClick}
+            style={modalBtn}
+          />
         </div>
-      ) : (
-        ""
-      )}
+      </div>
     </div>
   );
 };
@@ -72,6 +60,7 @@ ModalDialog.propTypes = {
   containerModal: PropTypes.object,
   styleText: PropTypes.object,
   text: PropTypes.string,
+  handleClick: PropTypes.func,
 };
 
 export default ModalDialog;
